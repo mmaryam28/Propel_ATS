13c2b8312dc6854127408043ab10970e4292d5b9
m4lachi <mz376@njit.edu>
2025-12-17 01:49:18 -0500

    added APIDash

--- backend/src/email-integration/email-integration.module.ts
+++ backend/src/email-integration/email-integration.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { EmailIntegrationController } from './email-integration.controller';
 import { EmailIntegrationService } from './email-integration.service';
 import { SupabaseModule } from '../supabase/supabase.module';
+import { ApiMonitoringModule } from '../api-monitoring/api-monitoring.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [
+    SupabaseModule,
+    forwardRef(() => ApiMonitoringModule),
+  ],
   controllers: [EmailIntegrationController],
   providers: [EmailIntegrationService],
   exports: [EmailIntegrationService],
--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
 import { ApplicationAnalyticsController } from './application-analytics.controller';
 import { ApplicationAnalyticsService } from './application-analytics.service';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { SalaryModule } from '../salary/salary.module';
+import { ApiMonitoringModule } from '../api-monitoring/api-monitoring.module';
 
 @Module({
-  imports: [SupabaseModule, SalaryModule],
+  imports: [
+    SupabaseModule,
+    SalaryModule,
+    forwardRef(() => ApiMonitoringModule),
+  ],
   controllers: [JobsController, ApplicationAnalyticsController],
   providers: [JobsService, ApplicationAnalyticsService],
 })
